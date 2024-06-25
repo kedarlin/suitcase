311ff800b3b6e1ed32462254b78ed27bdc247b41
kedarlin <kedarkamaf@gmail.com>
2024-06-25 19:16:19 +0530

    modified grid layout and styles

--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -5,8 +5,8 @@ import './Work.css';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import AnimationOne from '../../Components/Animation-1/Animation_1';
-import AnimationTwo from '../../Components/Animation-2/Animation_2';
+// import AnimationOne from '../../Components/Animation-1/Animation_1';
+// import AnimationTwo from '../../Components/Animation-2/Animation_2';
 import SliderAnimation from '../../Components/SliderAnimation/SliderAnimation';
 import { FiArrowUpRight } from 'react-icons/fi';
 
@@ -82,22 +82,7 @@ const Work = () => {
                     </h1>
                 </div>
                 <div className='work-boxes'>
-                    <div className='work-box-project scrolleffectleftside'>
-                        <div className='work-box scrolleffect'>
-                            <img src='./Assets/box-img-1.png' alt='box-img' />
-                        </div>
-                        <div className='work-box-desc'>
-                            <div className='work-box-name'>
-                                <p className='work-box-title'>Suitcase App</p>
-                                <FiArrowUpRight size={20} />
-                            </div>
-                            <div className='work-box-name-h'>
-                                <p className='work-box-title'>Travel Smart and Safety</p>
-                                <FiArrowUpRight size={20} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='work-box-project scrolleffectrightside'>
+                    <div className='work-box-project scrolleffect'>
                         <div className='work-box scrolleffect'>
                             <img src='./Assets/box-img-1.png' alt='box-img' />
                         </div>
@@ -127,7 +112,7 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='work-box-project scrolleffectleftside'>
+                    <div className='work-box-project scrolleffect'>
                         <div className='work-box scrolleffect'>
                             <img src='./Assets/box-img-1.png' alt='box-img' />
                         </div>
@@ -142,7 +127,22 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='work-box-project scrolleffectrightside'>
+                    <div className='work-box-project scrolleffect'>
+                        <div className='work-box scrolleffect'>
+                            <img src='./Assets/box-img-1.png' alt='box-img' />
+                        </div>
+                        <div className='work-box-desc'>
+                            <div className='work-box-name'>
+                                <p className='work-box-title'>Suitcase App</p>
+                                <FiArrowUpRight size={20} />
+                            </div>
+                            <div className='work-box-name-h'>
+                                <p className='work-box-title'>Travel Smart and Safety</p>
+                                <FiArrowUpRight size={20} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='work-box-project scrolleffect'>
                         <div className='work-box scrolleffect'>
                             <img src='./Assets/box-img-1.png' alt='box-img' />
                         </div>
